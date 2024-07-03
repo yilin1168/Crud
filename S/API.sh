cbcb7bf8719b23677e6efc07f48d79036a24d480
Hello <43439248+yilin1168@users.noreply.github.com>
2024-07-03 10:37:56 +0800

    Create API.sh

---
+++ S/API.sh
@@ -0,0 +1,3 @@
+curl --location 'https://api.bnm.gov.my/public/exchange-rate' \
+--header 'Accept: application/vnd.BNM.API.v1+json' \
+--header 'Cookie: 6faa1a9cb04f644c6b76dd2d6810bb5a=7d43fa7fde710f0b9c2a73eb39e95069; TS01efcbd3=012be42521c740f6c6955c227af6dc6852a76277101acade3e83ae60ac8b0b89c518b8ab8fcc047ca141ad1bd899027e9449d23307929d3b80e35a12d9b85a914ced9090b1'
